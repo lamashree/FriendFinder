c206c3e0d2313f8c84fe7c81e03b64c8dab59763
admin <admin@admins-MacBook-Pro.local>
2019-11-22 12:01:41 -0800

    added initial file

---
+++ server.js
@@ -0,0 +1,19 @@
+var bodyParser = require('body-parser')
+
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
+  
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+ 
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+ 
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
