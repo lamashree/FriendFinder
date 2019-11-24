cd398f378f1a98c4d9bdf8119415f1baa8fc1641
admin <admin@admins-MacBook-Pro.local>
2019-11-23 19:28:54 -0800

    added content for friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,25 +1,41 @@
-var tableArray = [
+var friendData = [
     {
-        name: 'Smith',
-        photo:"https//media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        score: [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-    
-        ]
-    }
+       "name": 'Smith',
+        "photo":"https//media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "score": [ 5, 1,4, 4,5, 1, 2, 5, 4, 1]
+    },
+    {
+        "name": 'Robin',
+         "photo":"https//media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+         "score": [ 5, 1,4, 4,5, 2, 2, 3, 3, 5]
+     },
+     {
+        "name": 'tyler',
+         "photo":"https//media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+         "score": [ 2, 4,4, 4,1, 2, 1, 3, 1, 5]
+     },
+     {
+        "name": 'Brain',
+         "photo":"https//media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+         "score": [ 3, 3, 4, 4, 2, 2, 2, 3, 3, 5]
+     },
+     {
+        "name": 'Ellen',
+         "photo":"https//media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+         "score": [ 5, 5, 5, 5, 5, 2, 2, 3, 3, 5]
+     },
+
+     {
+        "name": 'stefan',
+         "photo":"https//media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+         "score": [ 1, 1, 1, 2, 2, 2, 2, 3, 3, 5]
+     },
+
+
 ]
 
 
 
 
 
-module.exports = tableArray;
+module.exports = friendData;
